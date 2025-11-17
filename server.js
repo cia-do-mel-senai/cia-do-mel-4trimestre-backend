b6869798d3392f39641726c14732be7b2b5c1ce4
victorcunhasc@gmail.com <victorcunhasc@gmail.com>
2025-11-17 20:08:08 -0300

    Atualização cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors("*"));
+app.use(cors({ origin: "*" }));
 
 const usuarioController = new UsuarioController();
 
